0fba665147c9a56f45f8bbe049d3b4407da2d958
Yoann Moreau <yoann.moreau@tutanota.com>
2026-07-20 01:18:38 +0200

    First approach: modify loot tables -> prevent wheat seeds from dropping when harvesting wheat with a hoe

--- src/main/java/fr/ethilvan/CropsMod.java
+++ src/main/java/fr/ethilvan/CropsMod.java
@@ -2,8 +2,17 @@ package fr.ethilvan;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.resources.Identifier;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +32,27 @@ public class CropsMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("crops-mod enabled");
+		modifyCropsDrops();
 	}
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+
+
+	private void modifyCropsDrops() {
+		LootTableEvents.MODIFY_DROPS.register((holder, context, drops) -> {
+
+			ResourceKey<LootTable> wheatTable = Blocks.WHEAT.getLootTable().orElseThrow();
+			ItemStack tool = (ItemStack) context.getOptionalParameter(LootContextParams.TOOL);
+
+			holder.unwrapKey().ifPresent(key -> {
+				if (key.equals(wheatTable)) {
+					if (tool != null && tool.is(ItemTags.HOES)) {
+						drops.removeIf(stack -> stack.is(Items.WHEAT_SEEDS));
+					}
+				}
+			});
+		});
 	}
 }
