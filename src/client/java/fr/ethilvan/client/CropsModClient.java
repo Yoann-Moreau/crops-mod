f044f443e1af6b2425133f373cf8a6eb56aeeea4
Yoann Moreau <yoann.moreau@tutanota.com>
2026-07-19 23:55:07 +0200

    Initial commit

---
+++ src/client/java/fr/ethilvan/client/CropsModClient.java
@@ -0,0 +1,12 @@
+package fr.ethilvan.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class CropsModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/fr/ethilvan/CropsMod.java
@@ -0,0 +1,31 @@
+package fr.ethilvan;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class CropsMod implements ModInitializer {
+	public static final String MOD_ID = "crops-mod";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("crops-mod enabled");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
